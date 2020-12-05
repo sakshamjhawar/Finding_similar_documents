0e713b5800c6eb93c5c7f630d41003bb99fca21c
Abhinaw Sarang <asarang@asu.edu>
2020-12-04 22:33:31 -0700

    Added Infobox

--- src/displayData.js
+++ src/displayData.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React from "react";
 
 class ContentDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            data :props.resultData
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.resultData,
+    };
+  }
 
-    render() {
-        return (
-            <div className="row">
-                <div className="column">
-                    {this.state.data.bindings.map(each => {
-    return <p key={each.PaperTitle.value}>{each.PaperTitle.value}</p>
-                    })}
-                </div>
-                {/* <div className="column">
-                    <h2>Column 2</h2>
-                    <p>Some text..</p>
-                </div> */}
-            </div>
-        );
+  customResult(array) {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
+    return array.slice(0, 9);
+  }
+
+  render() {
+    const resultData = this.customResult(this.state.data.bindings);
+    return (
+      <div className="row">
+        <div className="column">
+          {resultData.map((each) => {
+            return <p key={each.PaperTitle.value}>{each.PaperTitle.value}</p>;
+          })}
+        </div>
+       
+      </div>
+    );
+  }
 }
 export default ContentDisplay;
--- src/searchbox.js
+++ src/searchbox.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import axios from 'axios';
-import './searchbox.scss';
-import myText from './sample.txt';
-import ContentDisplay from './displayData'
-import qs from 'qs';
+import React from "react";
+import axios from "axios";
+import "./searchbox.scss";
+import myText from "./sample.txt";
+import ContentDisplay from "./displayData";
+import qs from "qs";
 
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
     //readingFile();
     //console.log("file is read");
-    this.state = { 
-      value: '',
-      tokens: '',
-      result: ''
+    this.state = {
+      value: "",
+      tokens: "",
+      result: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -24,97 +24,127 @@ class NameForm extends React.Component {
     this.setState({ value: event.target.value });
   }
 
-  matchTopic = () => this.state.tokens.filter(s => (this.state.value.split(' ')[0] || this.state.value.split(' ')[1]).includes(s.id) || s.id.includes(this.state.value.split(' ')[0] || this.state.value.split(' ')[1]))
+  matchTopic = () =>
+    this.state.tokens.filter(
+      (s) =>
+        (
+          this.state.value.split(" ")[0] || this.state.value.split(" ")[1]
+        ).includes(s.id) ||
+        s.id.includes(
+          this.state.value.split(" ")[0] || this.state.value.split(" ")[1]
+        )
+    );
 
-    handleClick = (event) => {
+  handleClick = (event) => {
     event.preventDefault();
+    this.setState({ result: "" });
     fetch(myText)
-    .then((r) => r.text())
-    .then((text)  => {
-      var splitStn = text.split('\n');
-      splitStn.forEach( (row) => {
-        var splitStr = row.split(',');
-        var word = splitStr[0];
-        var val = splitStr[1];
-        this.setState({
-          tokens : [...this.state.tokens,  {id : word.trim().replace(/^"|"$/g, ''),  value: val} ]
+      .then((r) => r.text())
+      .then((text) => {
+        var splitStn = text.split("\n");
+        splitStn.forEach((row) => {
+          var splitStr = row.split(",");
+          var word = splitStr[0];
+          var val = splitStr[1];
+          this.setState({
+            tokens: [
+              ...this.state.tokens,
+              { id: word.trim().replace(/^"|"$/g, ""), value: val },
+            ],
+          });
         });
       })
-    })
-    .then(() => {
-      console.log(event);
-      this.handleSubmit(event);
-    });
-  } 
+      .then(() => {
+        console.log(event);
+        this.handleSubmit(event);
+      });
+  };
 
   handleSubmit = (event) => {
     //alert(event.target.childNodes[1].value);
     this.setState({
-      value : event.target.childNodes[1].value
-    })
-    var topicObject = this.matchTopic() //Get from tokens
-    console.log(topicObject)
-    var topicNumber = topicObject[0].value
+      value: event.target.childNodes[1].value,
+    });
+    var topicObject = this.matchTopic(); //Get from tokens
+    console.log(topicObject);
+    var topicNumber = topicObject[0].value;
     console.log(topicNumber);
-    if (topicNumber === '0') {
-      axios( {
-        method : 'POST',
-        url : 'http://ec2-35-162-161-210.us-west-2.compute.amazonaws.com:3030/sampleDatasetRDF/', 
-        headers : { 'content-type' : 'application/x-www-form-urlencoded' },
-        data : qs.stringify({ 
-          query : 'PREFIX type: <http://info.deepcarbon.net/schema/type#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?PaperTitle ?PaperAuthor ?PaperPages WHERE { ?s a <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Document>;             <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Topic_match_1> ?matchPercent; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_title> ?PaperTitle;  <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_author> ?PaperAuthor; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_pages> ?PaperPages. FILTER(xsd:float(?matchPercent) >= "0.5"^^xsd:float)  }'
-        })
-      })
-      .then((res) => {
-        this.setState({ result: res.data.results});
-      })
-
-    } else if (topicNumber === '1') {
-      axios( {
-        method : 'POST',
-        url : 'http://ec2-35-162-161-210.us-west-2.compute.amazonaws.com:3030/sampleDatasetRDF/', 
-        headers : { 'content-type' : 'application/x-www-form-urlencoded' },
-        data : qs.stringify({ 
-          query : 'PREFIX type: <http://info.deepcarbon.net/schema/type#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?PaperTitle ?PaperAuthor ?PaperPages WHERE { ?s a <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Document>;             <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Topic_match_2> ?matchPercent; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_title> ?PaperTitle;  <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_author> ?PaperAuthor; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_pages> ?PaperPages. FILTER(xsd:float(?matchPercent) >= "0.5"^^xsd:float)  }'
-        })
-      })
-      .then((res) => {
-        this.setState({ result: res.data.results});
-      })
-    } else if (topicNumber === '2') {
-      axios( {
-        method : 'POST',
-        url : 'http://ec2-35-162-161-210.us-west-2.compute.amazonaws.com:3030/sampleDatasetRDF/', 
-        headers : { 'content-type' : 'application/x-www-form-urlencoded' },
-        data : qs.stringify({ 
-          query : 'PREFIX type: <http://info.deepcarbon.net/schema/type#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?PaperTitle ?PaperAuthor ?PaperPages WHERE { ?s a <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Document>;             <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Topic_match_3> ?matchPercent; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_title> ?PaperTitle;  <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_author> ?PaperAuthor; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_pages> ?PaperPages. FILTER(xsd:float(?matchPercent) >= "0.5"^^xsd:float)  }'
-        })
-      })
-      .then((res) => {
-        this.setState({ result: res.data.results});
-      })
+    if (topicNumber === "0") {
+      axios({
+        method: "POST",
+        url:
+          "http://ec2-35-162-161-210.us-west-2.compute.amazonaws.com:3030/sampleDatasetRDF/",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        data: qs.stringify({
+          query:
+            'PREFIX type: <http://info.deepcarbon.net/schema/type#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?PaperTitle ?PaperAuthor ?PaperPages WHERE { ?s a <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Document>;             <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Topic_match_1> ?matchPercent; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_title> ?PaperTitle;  <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_author> ?PaperAuthor; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_pages> ?PaperPages. FILTER(xsd:float(?matchPercent) >= "0.6"^^xsd:float)  }',
+        }),
+      }).then((res) => {
+        this.setState({ result: res.data.results });
+      });
+    } else if (topicNumber === "1") {
+      axios({
+        method: "POST",
+        url:
+          "http://ec2-35-162-161-210.us-west-2.compute.amazonaws.com:3030/sampleDatasetRDF/",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        data: qs.stringify({
+          query:
+            'PREFIX type: <http://info.deepcarbon.net/schema/type#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?PaperTitle ?PaperAuthor ?PaperPages WHERE { ?s a <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Document>;             <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Topic_match_2> ?matchPercent; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_title> ?PaperTitle;  <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_author> ?PaperAuthor; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_pages> ?PaperPages. FILTER(xsd:float(?matchPercent) >= "0.6"^^xsd:float)  }',
+        }),
+      }).then((res) => {
+        this.setState({ result: res.data.results });
+      });
+    } else if (topicNumber === "2") {
+      axios({
+        method: "POST",
+        url:
+          "http://ec2-35-162-161-210.us-west-2.compute.amazonaws.com:3030/sampleDatasetRDF/",
+        headers: { "content-type": "application/x-www-form-urlencoded" },
+        data: qs.stringify({
+          query:
+            'PREFIX type: <http://info.deepcarbon.net/schema/type#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> SELECT ?PaperTitle ?PaperAuthor ?PaperPages WHERE { ?s a <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Document>;             <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#Topic_match_3> ?matchPercent; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_title> ?PaperTitle;  <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_author> ?PaperAuthor; <http://www.semanticweb.org/sarang/ontologies/2020/10/untitled-ontology-11#has_pages> ?PaperPages. FILTER(xsd:float(?matchPercent) >= "0.6"^^xsd:float)  }',
+        }),
+      }).then((res) => {
+        this.setState({ result: res.data.results });
+      });
     }
-  }
+  };
 
   render() {
     return (
       <div>
-      <div className="mainFrame">
-        <form onSubmit={this.handleClick}  role="search">
-          <label htmlFor="search">Search Topic</label>
-          <input id="search" type="search" placeholder="Search Topic..." autoFocus required />
-          <button type="submit">Go</button>
-        </form>
+        <div className="mainFrame">
+          <form onSubmit={this.handleClick} role="search">
+            <label htmlFor="search">Search Topic</label>
+            <input
+              id="search"
+              type="search"
+              placeholder="Search Topic..."
+              autoFocus
+              required
+            />
+            <button type="submit">Go</button>
+          </form>
+        </div>
+        <div className="row">
+          <div className="column">
+            <div>
+              <h1>Search result</h1>
+            </div>
+            <div>
+              {this.state.result ? (
+                <ContentDisplay resultData={this.state.result} />
+              ) : (
+                <div></div>
+              )}
+            </div>
+          </div>
+          <div className="column">
+            <p>Second Column</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1>Search result</h1>
-      </div>
-      <div>
-        {(this.state.result) ? <ContentDisplay resultData={this.state.result}/> : <div></div>}
-      </div>
-      </div>
-
-
     );
   }
 }
